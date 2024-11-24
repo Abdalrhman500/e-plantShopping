571db270344658170ab9b3045593cb49371306f2
cazcoder <phpworkbench@gmail.com>
2024-11-24 11:21:52 +0200

    Added product price

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -269,7 +269,7 @@ function ProductList() {
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
+               <a href="#" onClick={(e)=>handlePlantsClick(e)} style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
@@ -298,6 +298,7 @@ function ProductList() {
                             <div className="product-card" key={plantIndex}>
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
+                                <div className="product-cost">{plant.cost}</div>
                                 {isProductInCart(plant.name) ?
                                   <button className="product-button added-to-cart">Added to Cart</button>
                                 : <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button> 

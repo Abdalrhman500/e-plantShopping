26517c8a6fc124d0842dbe6653908c7430d59f06
cazcoder <phpworkbench@gmail.com>
2024-11-24 11:44:47 +0200

    Changing number of items in cart.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -263,6 +263,14 @@ function ProductList() {
         return itemFound ? true : false;
     };
 
+    const getProductsInCart = () => {
+        let totalPlants = 0;
+        cart.forEach((item) => {
+            totalPlants += item.quantity;
+        });
+        return totalPlants;
+    }
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -282,7 +290,7 @@ function ProductList() {
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
                 <div> 
                   <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
-                   <div className="cart_quantity_count">{cart.length}</div>
+                   <div className="cart_quantity_count">{getProductsInCart()}</div>
                    <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1>
                   </a>
                 </div>
